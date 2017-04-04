9e6658ba3e87d1846113ff0ba9629a1cb3b1562f
Alex Bierach <alex@thebrandingfarm.com>
2017-04-04 15:44:03 -0700

    add intial footer markup and styling

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 
 html, body, #app, #app > div {
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="footer">
-    <div class="disclaimer">
-      <p>This program is meant for educational purposes only. If you are in crisis or you think you may have an emergency, call your doctor or 911 immediately.</p>
-    </div>
+  <div class="footer container-fluid">
+    <div class="row">
+      <div class="footer-block col-xs-12 col-sm-4">
+        <h3>Help is Available</h3>
+        <div>
+          <p>In Los Angeles County, visit the <a target="_blank" href="http://directory.maternalmentalhealthnow.org/">Maternal Mental Health Resource Directory</a>.&nbsp; Outside of Los Angeles County, visit <a href="http://www.postpartum.net">Postpartum Support International</a> or call 1-800-944-4773.</p></div>
+        </div>
+        <div class="footer-block col-xs-12 col-sm-4">
+          <h3>Maternal Mental Health Now</h3>
+          <ul>
+            <li><a href="http://www.maternalmentalhealthnow.org/index.php/about-us">About Us</a></li>
+            <li><a href="http://www.maternalmentalhealthnow.org/index.php/news">News</a></li>
+            <li><a href="http://www.maternalmentalhealthnow.org/index.php/calendar">Calendar</a></li>
+          </ul>
+        </div>
+      </div>
   </div>
+
 </template>
 
 <script>
@@ -13,10 +26,45 @@
 
 <style>
 .footer {
-  height: 150px;
+  min-height: 180px;
   background-color: #090D0F;
   color: #ffffff;
   padding: 5% 10%;
+  line-height: 24px;
+}
+
+.footer-block {
+  max-width: 350px;
+}
+
+p {
+  color: #686868;
+  font-size: 14px;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.footer h3 {
+  text-transform: uppercase;
+  color: #cbce00;
+  font-size: 14px;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+.footer a {
+  color: #a8a8a8;
+  font-size: 14px;
+  text-decoration: none;
+
+}
+.footer a:hover {
+  color: #cbce00;
+  text-decoration: none;
 }
 
 </style>
