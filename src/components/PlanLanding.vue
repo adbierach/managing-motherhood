abb4957df6c517e6368a6220984c5193009f0dbc
Alex Bierach <alex@thebrandingfarm.com>
2017-06-29 14:08:15 -0700

    Add option to delete care plan as a temp fix

--- src/components/PlanLanding.vue
+++ src/components/PlanLanding.vue
@@ -8,6 +8,7 @@
           <p>It’s helpful to have a plan in place to help when you are feeling overwhelmed, upset, depressed, anxious or just stressed.</p>
           <router-link :to="'/plan/build'" class="btn btn-primary btn-lg" role="button">Create Plan</router-link>
           <router-link v-if="carePlanExists" :to="'/plan/view'" class="btn btn-primary btn-lg" role="button">View Plan</router-link>
+          <p v-if="carePlanExists" v-on:click="deletePlan">Delete Care Plan</p>
         </div>
       </div>
     </div>
@@ -27,7 +28,15 @@ export default {
       carePlanExists: !!window.localStorage.getItem('plan')
     }
   },
-  components: {'site-header': Header, 'site-footer': Footer}
+  components: {'site-header': Header, 'site-footer': Footer},
+  methods: {
+    deletePlan: function (e) {
+      window.localStorage.removeItem('plan')
+      // doing this because the above is not watching localstorage
+      // there is probably a better way to do this
+      this.carePlanExists = false
+    }
+  }
 }
 </script>
 
